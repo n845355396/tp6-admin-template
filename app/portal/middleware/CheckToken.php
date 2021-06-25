68a619e3d2891e5921d96479bbbeeba9893defe1
lpc <845355396@qq.com>
2021-06-25 18:40:42 +0800

    前端接口验证token去掉密码一致性验证操作

--- app/portal/middleware/CheckToken.php
+++ app/portal/middleware/CheckToken.php
@@ -30,7 +30,7 @@ class CheckToken
 
         $request->userInfo = $result['data']['data'];
         $request->userId   = $result['data']['data']->user_id;
-        $password           = $result['data']['data']->password;
+        $password          = $result['data']['data']->password;
 
         //@Author: lpc @Description: 前台的用户不验证密码是否一致了，因为要查询数据库，我怕在一些情况下数据库会出问题 @DateTime: 2021/6/25 18:38
         //lpc 这里我们来判断下用户密码是否被修改，修改后就算token正确都要重新登录
